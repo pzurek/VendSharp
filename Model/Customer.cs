19f00ca3f367c0ac091a58352f65382d48c61bc4
Piotr Zurek <p.zurek@gmail.com>
2013-08-05 20:12:21 +1200

    Chages the Customer class based on real output from API

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -8,10 +8,10 @@ namespace Vend
 	public class Customer : BaseApiObject
 	{
 		/// <summary>
-		/// Gets or sets the retailer identifier.
+		/// Gets or sets the name.
 		/// </summary>
-		/// <value>The retailer identifier.</value>
-		public string RetailerId { get; set; }
+		/// <value>The name.</value>
+		public string Name { get; set; }
 
 		/// <summary>
 		/// A short string code assigned to customers by the retailer.
@@ -21,22 +21,184 @@ namespace Vend
 		public string CustomerCode { get; set; }
 
 		/// <summary>
+		/// Gets or sets the customer group identifier.
+		/// </summary>
+		/// <value>The customer group identifier.</value>
+		public string CustomerGroupId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the customer group.
+		/// </summary>
+		/// <value>The name of the customer group.</value>
+		public string CustomerGroupName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the first name.
+		/// </summary>
+		/// <value>The first name.</value>
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the last name.
+		/// </summary>
+		/// <value>The last name.</value>
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the company.
+		/// </summary>
+		/// <value>The name of the company.</value>
+		public string CompanyName { get; set; }
+
+		/// <summary>
+		/// Gets or sets the phone.
+		/// </summary>
+		/// <value>The phone.</value>
+		public string Phone { get; set; }
+
+		/// <summary>
+		/// Gets or sets the mobile.
+		/// </summary>
+		/// <value>The mobile.</value>
+		public string Mobile { get; set; }
+
+		/// <summary>
+		/// Gets or sets the fax.
+		/// </summary>
+		/// <value>The fax.</value>
+		public string Fax { get; set; }
+
+		/// <summary>
+		/// Gets or sets the email.
+		/// </summary>
+		/// <value>The email.</value>
+		public string Email { get; set; }
+
+		/// <summary>
+		/// Gets or sets the twitter.
+		/// </summary>
+		/// <value>The twitter.</value>
+		public string Twitter { get; set; }
+
+		/// <summary>
+		/// Gets or sets the website.
+		/// </summary>
+		/// <value>The website.</value>
+		public string Website {get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical address1.
+		/// </summary>
+		/// <value>The physical address1.</value>
+		public string PhysicalAddress1 { get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical address2.
+		/// </summary>
+		/// <value>The physical address2.</value>
+		public string PhysicalAddress2 { get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical suburb.
+		/// </summary>
+		/// <value>The physical suburb.</value>
+		public string PhysicalSuburb { get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical city.
+		/// </summary>
+		/// <value>The physical city.</value>
+		public string PhysicalCity { get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical postcode.
+		/// </summary>
+		/// <value>The physical postcode.</value>
+		public string PhysicalPostcode { get; set; }
+
+		/// <summary>
+		/// Gets or sets the state of the physical.
+		/// </summary>
+		/// <value>The state of the physical.</value>
+		public string PhysicalState { get; set; }
+
+		/// <summary>
+		/// Gets or sets the physical country identifier.
+		/// </summary>
+		/// <value>The physical country identifier.</value>
+		public string PhysicalCountryId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal address1.
+		/// </summary>
+		/// <value>The postal address1.</value>
+		public string PostalAddress1 { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal address2.
+		/// </summary>
+		/// <value>The postal address2.</value>
+		public string PostalAddress2 { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal suburb.
+		/// </summary>
+		/// <value>The postal suburb.</value>
+		public string PostalSuburb { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal city.
+		/// </summary>
+		/// <value>The postal city.</value>
+		public string PostalCity { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal postcode.
+		/// </summary>
+		/// <value>The postal postcode.</value>
+		public string PostalPostcode { get; set; }
+
+		/// <summary>
+		/// Gets or sets the state of the postal.
+		/// </summary>
+		/// <value>The state of the postal.</value>
+		public string PostalState { get; set; }
+
+		/// <summary>
+		/// Gets or sets the postal country identifier.
+		/// </summary>
+		/// <value>The postal country identifier.</value>
+		public string PostalCountryId { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="Vend.Customer"/> has loyalty enabled.
+		/// </summary>
+		/// <value><c>true</c> if loyalty enabled; otherwise, <c>false</c>.</value>
+		public bool LoyaltyEnabled { get; set; }
+
+		/// <summary>
+		/// Gets or sets the loyalty balance.
+		/// </summary>
+		/// <value>The loyalty balance.</value>
+		public float LoyaltyBalance { get; set; }
+
+		/// <summary>
+		/// Gets or sets the updated at.
+		/// </summary>
+		/// <value>The updated at.</value>
+		public DateTime UpdatedAt { get; set; }
+
+		/// <summary>
+		/// Gets or sets the deleted at.
+		/// </summary>
+		/// <value>The deleted at.</value>
+		public DateTime DeletedAt { get; set; }
+
+		/// <summary>
 		/// Gets or sets the balance.
 		/// </summary>
 		/// <value>The balance.</value>
 		public float Balance { get; set; }
-
-		/// <summary>
-		/// Gets or sets the points.
-		/// </summary>
-		/// <value>The points.</value>
-		public int Points { get; set; }
-
-		/// <summary>
-		/// Gets or sets the note.
-		/// </summary>
-		/// <value>The note.</value>
-		public string Note { get; set; }
 
 		/// <summary>
 		/// Gets or sets the year to date spending.
@@ -45,16 +207,16 @@ namespace Vend
 		public float YTD { get; set; }
 
 		/// <summary>
-		/// Gets or sets the sex.
-		/// </summary>
-		/// <value>The sex.</value>
-		public string Sex { get; set; }
-
-		/// <summary>
 		/// Gets or sets the date of birth.
 		/// </summary>
 		/// <value>The date of birth.</value>
 		public string DOB { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the sex.
+		/// </summary>
+		/// <value>The sex.</value>
+		public string Sex { get; set; }
 
 		/// <summary>
 		/// Gets or sets the custom field.
@@ -81,43 +243,9 @@ namespace Vend
 		public string CustomField4 { get; set; }
 
 		/// <summary>
-		/// Gets or sets a value indicating whether this <see cref="Vend.Customer"/> has loyalty enabled.
-		/// </summary>
-		/// <value><c>true</c> if loyalty enabled; otherwise, <c>false</c>.</value>
-		public bool LoyaltyEnabled { get; set; }
-
-		/// <summary>
-		/// Gets or sets the loyalty balance.
-		/// </summary>
-		/// <value>The loyalty balance.</value>
-		public float LoyaltyBalance { get; set; }
-
-		/// <summary>
-		/// Gets or sets the updated at.
-		/// </summary>
-		/// <value>The updated at.</value>
-		public DateTime UpdatedAt { get; set; }
-
-		/// <summary>
-		/// Gets or sets the created at.
-		/// </summary>
-		/// <value>The created at.</value>
-		public DateTime CreatedAt { get; set; }
-
-		/// <summary>
-		/// Gets or sets the contact.
+		/// Gets or sets the <see cref="Vend.CustomerContact"/>
 		/// </summary>
 		/// <value>The contact.</value>
-		public Contact Contact { get; set; }
-
-		public string FirstName {
-			get { return Contact.FirstName; }
-			set { Contact.FirstName = value; }
-		}
-
-		public string LastName {
-			get { return Contact.LastName; }
-			set { Contact.LastName = value; }
-		}
+		public CustomerContact Contact { get; set; }
 	}
 }
