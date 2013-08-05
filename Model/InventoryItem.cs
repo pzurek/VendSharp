7759d624b999fc4e6b1201882ca5f87c33b6f685
Piotr Zurek <p.zurek@gmail.com>
2013-08-05 21:02:55 +1200

    Adds new classes matching API output

---
+++ Model/InventoryItem.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Vend
+{
+	/// <summary>
+	/// Inventory item.
+	/// </summary>
+	public class InventoryItem : BaseApiObject
+	{
+		/// <summary>
+		/// Gets or sets the product identifier.
+		/// </summary>
+		/// <value>The product identifier.</value>
+		public string ProductId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the outlet identifier.
+		/// </summary>
+		/// <value>The outlet identifier.</value>
+		public string OutletId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the attributed cost.
+		/// </summary>
+		/// <value>The attributed cost.</value>
+		public float AttributedCost { get; set; }
+
+		/// <summary>
+		/// Gets or sets the count.
+		/// </summary>
+		/// <value>The count.</value>
+		public int Count { get; set; }
+
+		/// <summary>
+		/// Gets or sets the reorder point.
+		/// </summary>
+		/// <value>The reorder point.</value>
+		public int ReorderPoint { get; set; }
+
+		/// <summary>
+		/// Gets or sets the restock level.
+		/// </summary>
+		/// <value>The restock level.</value>
+		public int RestockLevel { get; set; }
+	}
+}
