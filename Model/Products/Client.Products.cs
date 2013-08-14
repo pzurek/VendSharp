6ee73413087fb926bdcdfd82d029b9acd767c61b
Piotr Zurek <p.zurek@gmail.com>
2013-08-14 19:28:42 +1200

    Adds support for pagination for Products

--- Model/Products/Client.Products.cs
+++ Model/Products/Client.Products.cs
@@ -6,11 +6,33 @@ namespace Vend
 {
 	public partial class Client
 	{
+		// The public method used to retrieve the first page
 		public List<Product> GetProducts()
 		{
 			const string resourceName = "products";
-			var request = new RestRequest(resourceName, Method.GET);
-			var products = Execute<ProductListPage>(request).Products;
+			var products = GetProducts(resourceName);
+			return products;
+		}
+
+		// Private method used to retrieve 2+ pages
+		List<Product> GetProducts(int page)
+		{
+			string pageResourceName = string.Format("products/?page={0}", page.ToString());
+			var products = GetProducts(pageResourceName);
+			return products;
+		}
+
+		// Generic method used by the both above
+		// recursively grabs subsequent pages
+		List<Product> GetProducts(string pageResourceName)
+		{
+			var request = new RestRequest(pageResourceName, Method.GET);
+			var response = Execute<ProductListPage>(request);
+			var products = response.Products;
+			var pagination = response.Pagination;
+			if (pagination.Pages > pagination.Page) {
+				products.AddRange(GetProducts(pagination.Page + 1));
+			}
 			return products;
 		}
 	}
